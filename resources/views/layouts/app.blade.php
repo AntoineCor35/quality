aa5fd2ee366f8b71dd841ae1b113de0f8a3b5434
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-04-14 12:05:21 +0200

    Removed unsecured cdn resource

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,9 +72,6 @@
 
         @yield('content')
     </div>
-
-    <!-- Bootstrap JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
